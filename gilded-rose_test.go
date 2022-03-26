426b1c3e1e7ee0f3a4582d0d4898e7f48d5dd284
Víctor Huang Huang <vic-16@hotmail.com>
2022-03-26 14:40:27 +0100

    Unit test fixed, go.mod added

--- gilded-rose_test.go
+++ gilded-rose_test.go
@@ -9,7 +9,7 @@ func Test_Foo(t *testing.T) {
 
 	UpdateQuality(items)
 
-	if items[0].name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
+	if items[0].name != "foo" {
+		t.Errorf("Name: Expected %s but got %s ", "foo", items[0].name)
 	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gilded-rose-kata-go
+
+go 1.18
