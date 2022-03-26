bf7bbe1d8b5f9a980078ed994028db49805182fd
Víctor Huang Huang <vic-16@hotmail.com>
2022-03-26 18:44:31 +0100

    Added basic sell in value test

--- gilded-rose_test.go
+++ gilded-rose_test.go
@@ -1,15 +1,26 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_Foo(t *testing.T) {
-	var items = []*Item{
-		{"foo", 0, 0},
+func TestLowerSellInDate(t *testing.T) {
+	tests := []struct {
+		name string
+		item *Item
+	}{
+		// TODO: Add test cases.
+		{"happy path", &Item{"foo", 0, 0}},
+		{"another happy path", &Item{"bar", 10, 0}},
 	}
-
-	UpdateQuality(items)
-
-	if items[0].name != "foo" {
-		t.Errorf("Name: Expected %s but got %s ", "foo", items[0].name)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			originalItem := *tt.item
+			UpdateQuality([]*Item{tt.item})
+			// Only lowered by one
+			if originalItem.sellIn-tt.item.sellIn != 1 {
+				t.Errorf("Item %s: want %v, got %v", tt.item.name, originalItem.sellIn-1, tt.item.sellIn)
+			}
+		})
 	}
 }
