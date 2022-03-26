b79288d280fea1a20cc8b9caa44dd1ca6f70185a
Víctor Huang Huang <vic-16@hotmail.com>
2022-03-26 20:39:29 +0100

    UpdateQuality refactoring

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 Kata challenge attempt. Go files copied from go folder in [here](https://github.com/emilybache/GildedRose-Refactoring-Kata).
 
-## Notes
+## Approach
 
-* After some deliberation, I'm going with Table Driven Tests for the sake of simplicity, but I was thinking on separating them by product and/or Item field
+After some deliberation, I'm going with Table Driven Tests for the sake of simplicity, but it's probably better to split them by product and/or Item field in this case.
+
+The last commit is a refactor of the function Update quality. I left commented the original one with the "Conjured" item update for faster checking. Both should work.
+
+### Refactoring UpdateQuality function
+
+Since there are very few items with specific conditions and quality variations are very small, I decided to use and reuse small incremental functions, but ideally there should be a parameter in the function signature with the value that is going to be added or substracted. It would also leave just 2 extra functions instead of the current 3 and simplify a bit switch-case part. This way would allow easier refactoring and/or updating functionality.
 
 ## Environment
 
-* Using WSL2 with Ubuntu
+* WSL2 with Ubuntu
 * Go version -> 1.18
+* Visual Studio Code
--- gilded-rose.go
+++ gilded-rose.go
@@ -5,60 +5,130 @@ type Item struct {
 	sellIn, quality int
 }
 
+const (
+	AgedBrie        = "Aged Brie"
+	Sulfuras        = "Sulfuras, Hand of Ragnaros"
+	BackstagePasses = "Backstage passes to a TAFKAL80ETC concert"
+	Conjured        = "Conjured"
+)
+
+// func UpdateQuality(items []*Item) {
+// 	for i := 0; i < len(items); i++ {
+
+// 		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+// 			if items[i].quality > 0 {
+// 				if items[i].name != "Sulfuras, Hand of Ragnaros" {
+// 					items[i].quality = items[i].quality - 1
+// 				}
+// 				if items[i].name == "Conjured" && items[i].quality > 0 {
+// 					items[i].quality = items[i].quality - 1
+// 				}
+// 			}
+// 		} else {
+// 			if items[i].quality < 50 {
+// 				items[i].quality = items[i].quality + 1
+// 				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+// 					if items[i].sellIn < 11 {
+// 						if items[i].quality < 50 {
+// 							items[i].quality = items[i].quality + 1
+// 						}
+// 					}
+// 					if items[i].sellIn < 6 {
+// 						if items[i].quality < 50 {
+// 							items[i].quality = items[i].quality + 1
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+
+// 		if items[i].name != "Sulfuras, Hand of Ragnaros" {
+// 			items[i].sellIn = items[i].sellIn - 1
+// 		}
+
+// 		if items[i].sellIn < 0 {
+// 			if items[i].name != "Aged Brie" {
+// 				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+// 					if items[i].quality > 0 {
+// 						if items[i].name != "Sulfuras, Hand of Ragnaros" {
+// 							items[i].quality = items[i].quality - 1
+// 						}
+// 						if items[i].name == "Conjured" && items[i].quality > 0 {
+// 							items[i].quality = items[i].quality - 1
+// 						}
+// 					}
+// 				} else {
+// 					items[i].quality = items[i].quality - items[i].quality
+// 				}
+// 			} else {
+// 				if items[i].quality < 50 {
+// 					items[i].quality = items[i].quality + 1
+// 				}
+// 			}
+// 		}
+// 	}
+
+// }
+
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
 
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-				if items[i].name == "Conjured" && items[i].quality > 0 {
-					items[i].quality = items[i].quality - 1
-				}
+	for _, item := range items {
+
+		switch item.name {
+
+		case AgedBrie:
+			decreaseSellIn(item)
+			increaseQuality(item)
+			if item.sellIn < 0 {
+				increaseQuality(item)
 			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
-		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-						if items[i].name == "Conjured" && items[i].quality > 0 {
-							items[i].quality = items[i].quality - 1
-						}
+		case BackstagePasses:
+			decreaseSellIn(item)
+			if item.sellIn >= 0 {
+				increaseQuality(item)
+				if item.sellIn <= 10 {
+					increaseQuality(item)
+					if item.sellIn <= 5 {
+						increaseQuality(item)
 					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
 				}
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				if item.quality != 0 {
+					item.quality = 0
 				}
 			}
+
+		case Sulfuras:
+			continue
+
+		case Conjured:
+			decreaseSellIn(item)
+			decreaseQuality(item)
+			decreaseQuality(item)
+
+		default:
+			decreaseSellIn(item)
+			decreaseQuality(item)
 		}
 	}
+}
 
+func decreaseQuality(item *Item) {
+	if item.quality > 0 {
+		item.quality--
+		if item.sellIn < 0 && item.quality > 0 {
+			item.quality--
+		}
+	}
+}
+
+func increaseQuality(item *Item) {
+	if item.quality < 50 {
+		item.quality++
+	}
+}
+
+func decreaseSellIn(item *Item) {
+	item.sellIn--
 }
